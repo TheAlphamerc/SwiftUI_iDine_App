bee799bba3017e1419f274c97839bd7e8ce63ffe
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-15 11:56:55 +0530

    polishing design with fonts and colors

--- SwiftUI_IDine/Supporting Views/MenuItemRow.swift
+++ SwiftUI_IDine/Supporting Views/MenuItemRow.swift
@@ -10,13 +10,33 @@ import SwiftUI
 
 struct MenuItemRow: View {
     var item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var body: some View {
         HStack{
             ImageStore.shared.image(name:item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray,lineWidth: 2))
             VStack(alignment:.leading){
                 Text(item.name)
-                Text("$\(item.price)")
+                    .font(.headline)
+                HStack{
+                     Text("$\(item.price)")
+                     Spacer()
+                     ForEach(item.restrictions, id: \.self){restrictions in
+                        Text(restrictions)
+                            .font(.caption)
+                            .fontWeight(.black)
+                            .padding(5)
+                            .background(self.colors[restrictions, default:.black])
+                            .clipShape(Circle())
+                            .foregroundColor(.white)
+                        
+                    }
+//                     .padding(5,.right)
+                }
+               
             }
+            
         }
     }
 }
