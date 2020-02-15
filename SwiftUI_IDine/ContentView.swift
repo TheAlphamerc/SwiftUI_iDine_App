912d188975f3168c101a13fc27a1cd025aa61cd6
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-15 09:49:14 +0530

    initial commit

---
+++ SwiftUI_IDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_IDine
+//
+//  Created by Ashwin Das on 15/02/20.
+//  Copyright © 2020 TheAlphamerc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
