b99049dc22717e60b73c59ba60ab5d60d63ae744
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-15 10:15:27 +0530

    models, extention and helper class added

---
+++ SwiftUI_IDine/Model/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  SwiftUI_IDine
+//
+//  Created by TheAlphamerc on 15/02/20.
+//  Copyright © 2020 TheAlphamerc. All rights reserved.
+//
+
+import Foundation
+
+class Order {
+    var items = [MenuItem]()
+
+    var total: Int {
+        if items.count > 0 {
+            return items.reduce(0) { $0 + $1.price }
+        } else {
+            return 0
+        }
+    }
+
+    func add(item: MenuItem) {
+        items.append(item)
+    }
+
+    func remove(item: MenuItem) {
+        if let index = items.firstIndex(of: item) {
+            items.remove(at: index)
+        }
+    }
+}
